ad39ef6dd3a10a73a7d31482c015046e70dab6b1
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-01-24 15:04:11 +0100

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,21 @@
+namespace Pnwscm60\ContaoPtimeBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Pnwscm60\ContaoPtimeBundle\ContaoHelloWorldBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoPtimeBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
