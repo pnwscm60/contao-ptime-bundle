a9dc3d5c5689778bc4cc6c930a724a68c98b1b74
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-01-24 15:20:48 +0100

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -4,7 +4,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Pnwscm60\ContaoPtimeBundle\ContaoHelloWorldBundle;
+use Pnwscm60\ContaoPtimeBundle\ContaoPtimeBundle;
 
 class Plugin implements BundlePluginInterface
 {
